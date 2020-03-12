69f163a302370da13469c2fd5567f369697accdd
Gerardo <SpearBrave>
2020-03-12 17:39:41 -0500

    1st

---
+++ Downloads/Martial-arts/Martial-arts/src/main/java/org/launchcode/Martial/arts/MartialArtsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Martial.arts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartialArtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartialArtsApplication.class, args);
+	}
+
+}
---
+++ Downloads/Martial-arts/Martial-arts/src/test/java/org/launchcode/Martial/arts/MartialArtsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Martial.arts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartialArtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
